5938ca4f7109a6e978922331548f168f5532cc6c
RayWP <raymondwp24@gmail.com>
2020-11-17 01:00:59 +0700

    commit example 1

--- ExampleUsage/sample 1.c
+++ ExampleUsage/sample 1.c
@@ -4,39 +4,43 @@
 int main()
 {
     StringContent header[3]; //table header
-    StringContent isikonten[2][2]; //table content row
+    StringContent isikonten[4][3]; //table content 4 row 3 column
     
-    int k;
+    int row;
     system("cls");
 
-    strcpy(header[0],"1234567890");
-    strcpy(header[1],"kol 2");
-    strcpy(header[2],"kol 3");
+    strcpy(header[0],"column 1");
+    strcpy(header[1],"column 2");
+    strcpy(header[2],"column 3");
 
-    printf("hello world\n");
+    // create row 1
     strcpy(isikonten[0][0],"isi 7");
     strcpy(isikonten[0][1],"isi 2");
-    // strcpy(isikonten[0][2],"isi 3");
-    printf("hello world\n");
+    strcpy(isikonten[0][2],"isi 3");
+
+    // create row 2
     strcpy(isikonten[1][0],"isi 4");
     strcpy(isikonten[1][1],"isi 5");
-    // strcpy(isikonten[1][2],"isi 6");
-    printf("hello world\n");
-    // StringContent SC = (isikonten+2);
-    printf("hello world\n");
-    printf("ini 1: %s\n",(*(isikonten+1))); //isi 2
-    printf("size: %d", sizeof(isikonten)/sizeof(StringContent));
-    // printf("ini 2: %s",*(*SC+1)+1); //isi 5
-    
-    // setTableAttribute(6,5,10,3);
-    //creating header
-    // tableHeader(header);    
-    // for(k=0; k<3; k++)
-    // {
-    //     //adding row
-    //     tableContent(3,isikonten,k);
-    // }
+    strcpy(isikonten[1][2],"isi 6");
 
+    // create row 3
+    strcpy(isikonten[2][0],"isi 1");
+    strcpy(isikonten[2][1],"isi 9");
+    strcpy(isikonten[2][2],"isi 10");
+
+    // create row 4
+    strcpy(isikonten[3][0],"isi 14");
+    strcpy(isikonten[3][1],"isi 94");
+    strcpy(isikonten[3][2],"isi 125");
+    
+    setTableAttribute(6,5,15,3);
+    tableHeader(header);    
+    for(row=0; row<3; row++)
+    {
+        //adding row
+        tableContent(3,isikonten[row], row);
+    }
+    
     getch();
     return 0;
 }
